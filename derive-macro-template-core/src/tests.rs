9e282f501d0f9b63b3af05158bd2303e9533ca8f
Nordgaren <nordgarentv@gmail.com>
2024-02-25 16:50:57 -0700

    fix test

--- derive-macro-template-core/src/tests.rs
+++ derive-macro-template-core/src/tests.rs
@@ -5,7 +5,7 @@ use quote::quote;
 
 #[test]
 fn test() {
-    let after = derive_macro_impl(quote!(), quote!());
+    let after = derive_macro_impl(quote!());
     assert_ne!(
         after.to_string(),
         ""
